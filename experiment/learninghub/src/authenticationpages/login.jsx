2b2303348b195a4dfc27956e0e7fc127cf83807d
Beshwanthsai <beshwanthsai2005@gmail.com>
2025-02-05 08:01:20 +0530

    made some changes in login.jsx and css has been modified

--- experiment/learninghub/src/authenticationpages/login.jsx
+++ experiment/learninghub/src/authenticationpages/login.jsx
@@ -1,37 +1,36 @@
 import './login.css'
-export default function Login() {
-    return (
-        <div>
 
-            <div className="container">
-                <input id="signup_toggle" type="checkbox" />
-                <form className="form">
-                    <div className="form_front">
-                        <div className="form_details">Login</div>
-                        <input type="text" className="input" placeholder="Username" />
-                        <input type="text" className="input" placeholder="Password" />
-                        <button className="btn">Login</button>
-                        <span className="switch">Don't have an account?
-                            <label for="signup_toggle" class="signup_tog">
-                                Sign Up
-                            </label>
-                        </span>
-                    </div>
-                    <div className="form_back">
-                        <div className="form_details">SignUp</div>
-                        <input type="text" className="input" placeholder="Firstname" />
-                        <input type="text" className="input" placeholder="Username" />
-                        <input type="text" className="input" placeholder="Password" />
-                        <input type="text" className="input" placeholder="Confirm Password" />
-                        <button className="btn">Signup</button>
-                        <span className="switch">Already have an account?
-                            <label for="signup_toggle" className="signup_tog">
-                                Sign In
-                            </label>
-                        </span>
-                    </div>
-                </form>
+export default function Login() {
+
+
+    
+
+    return (
+        <div className="container">
+            <input id="signup_toggle" type="checkbox" />
+            <div className="form">
+                <div className="form_front">
+                    <div className="form_details">Login</div>
+                    <input type="text" className="input" placeholder="Username" required />
+                    <input type="password" className="input" placeholder="Password" required />
+                    <button className="btn">Login</button>
+                    <span className="switch">
+                        Don't have an account?
+                        <label htmlFor="signup_toggle" className="signup_tog">Sign Up</label>
+                    </span>
+                </div>
+                <div className="form_back">
+                    <div className="form_details">Sign Up</div>
+                    <input type="text" className="input" placeholder="Name" required />
+                    <input type="email" className="input" placeholder="Email" required />
+                    <input type="password" className="input" placeholder="Password" required />
+                    <button className="btn">Sign Up</button>
+                    <span className="switch">
+                        Already have an account?
+                        <label htmlFor="signup_toggle" className="signup_tog">Login</label>
+                    </span>
+                </div>
             </div>
         </div>
-    );
+    )
 }
