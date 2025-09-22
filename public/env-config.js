4ef845370b4b57e8d1f6c4e92960648e79831608
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-22 16:30:53 +0530

    Update env-config.js

--- public/env-config.js
+++ public/env-config.js
@@ -1,4 +1,5 @@
 // This file will be copied to the output directory during build
 window.env = {
   GEMINI_API_KEY: 'AIzaSyDVz4SpQUrGydfnK46GLetpv_F8Iz3HwBI',
+  BACKEND_URL: 'http://3.87.77.146:8083' 
 };
