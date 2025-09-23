5c8bc1fb88e37ac5e6eb066b6012b1dfb39f957e
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-23 10:27:57 +0530

    Update BACKEND_URL to new EC2 IP address

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,5 @@
 // Update BACKEND_URL to your EC2 IP when deploying
 window.env = {
   GEMINI_API_KEY: 'AIzaSyDVz4SpQUrGydfnK46GLetpv_F8Iz3HwBI',
-  BACKEND_URL: 'http://50.17.70.148:8083'  // Replace <EC2_PUBLIC_IP> dynamically or manually
+  BACKEND_URL: 'http://54.82.203.74:8083'  // Replace <EC2_PUBLIC_IP> dynamically or manually
 };
