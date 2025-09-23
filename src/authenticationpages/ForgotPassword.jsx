01b4e8ca02e703963d272d7b927cc3fc8e2d169a
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-23 10:54:00 +0530

    Update ForgotPassword.jsx

--- src/authenticationpages/ForgotPassword.jsx
+++ src/authenticationpages/ForgotPassword.jsx
@@ -21,7 +21,7 @@ export default function ForgotPassword() {
             return;
         }
 
-        axios.post("http://54.82.203.74/forgot-password", { username })
+        axios.post("http://34.207.251.218/forgot-password", { username })
             .then(response => {
                 toast.success('Password reset instructions sent!', {
                     position: "top-right",
