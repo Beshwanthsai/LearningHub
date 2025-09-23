6bd43f9561f26c9ad518d9df6ea02bb0aef2e70d
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-23 09:50:41 +0530

    Change BACKEND_URL to placeholder for EC2 IP

--- public/env-config.js
+++ public/env-config.js
@@ -1,5 +1,5 @@
-// This file will be copied to the output directory during build
+// Update BACKEND_URL to your EC2 IP when deploying
 window.env = {
   GEMINI_API_KEY: 'AIzaSyDVz4SpQUrGydfnK46GLetpv_F8Iz3HwBI',
-  BACKEND_URL: 'http://54.234.67.251:8083' 
+  BACKEND_URL: 'http://<EC2_PUBLIC_IP>:8083'  // Replace <EC2_PUBLIC_IP> dynamically or manually
 };
