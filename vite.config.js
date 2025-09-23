79b379809029960f3ba11256691e08421e6fce45
Beshwanth Sai Katari <143738062+Beshwanthsai@users.noreply.github.com>
2025-09-23 10:43:46 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/learninghub-frontend/'  // For Tomcat deployment
+  base: '/'  // For Tomcat deployment
 })
